a972dff16ed4ac3a6a9527aa09debf3cb0049d2c
Gerba <gmlongobardi@hotmail.com>
2024-09-17 16:21:04 -0300

    V2.3

--- app/contacto/page.js
+++ app/contacto/page.js
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from '../ui/components/Contacto/contacto.module.css';
 import Logo from '../../public/images/jochyLogo.png';
+import Form from '../ui/components/Contacto/Form/Form';
 
 
 
@@ -11,25 +12,7 @@ const Comercializador = () => {
     <div className={styles.container}>
       <div className={styles.wrapper}>
         <h2 className={styles.title}>CONTACTO</h2>
-        <form className={styles.form} id='form'>
-          <div className={styles.formItem}>
-              <label className={styles.label}>Nombre y Apellido</label>
-              <input className={styles.input} type="text" name='name' required />
-          </div>
-          <div className={styles.formItem}>
-              <label className={styles.label}>E-Mail</label>
-              <input className={styles.input} type="email" name='email' required />
-          </div>
-          <div className={styles.formItem}>
-              <label className={styles.label}>Teléfono</label>
-              <input className={styles.input} type="number" name='phone' min={0} required  />
-          </div>
-          <div className={styles.formItem}>
-              <label className={styles.label}>Mensaje</label>
-              <textarea className={styles.textarea} type="text" name='message' required />
-          </div>
-          <button className={styles.button} type='submit' form='form' >ENVIAR</button>
-        </form>
+        <Form />
         <Image src={Logo} alt='jochy puertos logo' width={150} height={40} className={styles.logo} />
         <h3 className={styles.title2}>puertos@jochy.com.ar</h3>
         <h3 className={styles.title2}>+54 9 11 5996-6552</h3>
